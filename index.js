76531fd2fd7420b2cd1540750583a752406dce1c
Dickson Eneh <dickseneh99@gmail>
2022-06-06 15:40:54 +0200

    socialappcicd

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const cors = require("cors")
+const port = 1234
+const app = express()
+const mongoose = require("mongoose")
+mongoose.connect("mongodb+srv://socialapp:today@cluster0.xd5wm.mongodb.net/socialapp?").then(()=>{
+    console.log("connected")
+}).catch((error)=>{
+    console.log(error.message)
+})
+app.use(cors())
+app.use(express.json())
+app.get("/", (req, res)=>{
+    res.status(200).json({
+        message:"success"
+        })
+})
+app.listen(()=>{
+    console.log("connected", port)
+})
